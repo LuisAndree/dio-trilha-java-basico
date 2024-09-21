e02d894830da6eadf1238426a386de7e322c4828
Luís André <andre2016999@gmail.com>
2024-09-20 23:40:53 -0300

    criando o projeto para exemplo de construtores

---
+++ getters-setters/src/escola/Aluno.java
@@ -0,0 +1,31 @@
+package escola;
+
+public class Aluno {
+    private String nome;
+    private int idade;
+    private String sexo;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
---
+++ getters-setters/src/escola/Escola.java
@@ -0,0 +1,14 @@
+package escola;
+
+public class Escola {
+    public static void main(String[] args) {
+        Aluno andre = new Aluno();
+
+       andre.setNome("Andre");
+       andre.setIdade(26);
+       andre.setSexo("M");
+
+        System.out.println("nome: " + andre.getNome() + "\nidade: " + andre.getIdade() + "\nsexo: " + andre.getSexo());
+    }
+
+}
