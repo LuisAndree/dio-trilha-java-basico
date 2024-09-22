dbced54f46507df85d9683e504e72cfc8db6d3e9
Luís André <andre2016999@gmail.com>
2024-09-21 22:40:17 -0300

    exercicio-01

---
+++ exercicios-praticos/exercicio-01/animais-java/src/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    public void makeSound() {
+        System.out.println("O animal faz um som.");
+    }
+
+    public void move() {
+        System.out.println("O animal se move.");
+    }
+}
