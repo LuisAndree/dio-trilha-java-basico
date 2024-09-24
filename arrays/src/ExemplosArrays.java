24f2a5fd7a86d1f137d5fc362792cd785da0483c
Luís André <andre2016999@gmail.com>
2024-09-23 23:16:00 -0300

    criando o projeto para exemplo de arrays

---
+++ arrays/src/ExemplosArrays.java
@@ -0,0 +1,34 @@
+public class ExemplosArrays {
+
+    public void imprimeArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Conta[] minhasContas;
+        minhasContas = new Conta[10];
+
+        Conta contaNova = new Conta();
+        contaNova.numero = 12345;
+        contaNova.saldo = 10000;
+
+        minhasContas[0] = contaNova;
+
+        minhasContas[1] = new Conta();
+        minhasContas[1].saldo = 5000;
+
+        int[] idades = new int[10];
+        for (int i = 0; i < 10; i++) {
+            idades[i] = i * 10;
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(idades[i]);
+        }
+        for (int x : idades) {
+            System.out.println(x);
+        }
+
+    }
+}
