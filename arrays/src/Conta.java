e9c988d1ceae005f923d06dc8c39d8f6277efd92
Luís André <andre2016999@gmail.com>
2024-09-25 20:54:17 -0300

    criando o projeto para exemplo de arrays

--- arrays/src/Conta.java
+++ arrays/src/Conta.java
@@ -1,4 +1,12 @@
 public class Conta {
     int numero;
     int saldo;
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 }
--- arrays/src/ExemplosArrays.java
+++ arrays/src/ExemplosArrays.java
@@ -12,12 +12,12 @@ public class ExemplosArrays {
 
         Conta contaNova = new Conta();
         contaNova.numero = 12345;
-        contaNova.saldo = 10000;
+        contaNova.setSaldo(10000);
 
         minhasContas[0] = contaNova;
 
         minhasContas[1] = new Conta();
-        minhasContas[1].saldo = 5000;
+        minhasContas[1].setSaldo(5000);
 
         int[] idades = new int[10];
         for (int i = 0; i < 10; i++) {
