2d57f7bc86a9e0bc82402f6c4a719e6fb2a02ee4
Luís André <andre2016999@gmail.com>
2024-09-28 22:04:45 -0300

    atualizacao exercicio-13

---
+++ exercicios-praticos/exercicio-13/exercicio-livro-7.7/src/banco77/AtualizadorDeContas.java
@@ -0,0 +1,21 @@
+package banco77;
+
+public class AtualizadorDeContas {
+    private double saldoTotal = 0;
+    private double selic;
+
+    public AtualizadorDeContas(double selic) {
+        this.selic = selic;
+    }
+
+    public void roda(Conta c) {
+        System.out.println("Saldo antigo: " + c.getSaldo());
+        c.atualiza(selic);
+        System.out.println("Novo saldo: " + c.getSaldo());
+        this.saldoTotal += c.getSaldo();
+    }
+
+    public double getSaldoTotal() {
+        return saldoTotal;
+    }
+}
---
+++ exercicios-praticos/exercicio-13/exercicio-livro-7.7/src/banco77/Banco.java
@@ -0,0 +1,42 @@
+package banco77;
+
+public class Banco {
+    protected Conta contas[];
+    private int contador = 0;
+    private static int totalDeContas;
+
+    public Banco(int capacidadeDeContas) {
+        this.contas = new Conta[capacidadeDeContas];
+    }
+
+    public void setContas(Conta conta) {
+        if (contador >= this.contas.length) {
+            Conta[] novaArray = new Conta[this.contas.length * 2];
+            for (int i = 0; i < this.contas.length; i++) {
+                novaArray[i] = this.contas[i];
+            }
+            this.contas = novaArray;
+            this.contas[contador] = conta;
+            contador++;
+            totalDeContas++;
+        } else if (contador < this.contas.length) {
+            this.contas[contador] = conta;
+            contador++;
+            totalDeContas++;
+        }
+    }
+    
+    public void getContas() {
+        for (int i = 0; i < contador; i++) {
+            System.out.println(this.contas[i]);
+        }
+    }
+
+    public void getConta(int id) {
+        System.out.println(this.contas[id - 1]);
+    }
+
+    public int getQuantidadeDeContas() {
+        return Banco.totalDeContas;
+    }
+}
