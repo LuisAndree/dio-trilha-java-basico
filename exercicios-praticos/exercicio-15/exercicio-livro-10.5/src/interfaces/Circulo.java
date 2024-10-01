9c6f4b981f3494fcc708518adc9180e936ce5dcf
Luís André <andre2016999@gmail.com>
2024-09-30 22:14:02 -0300

    exercicio-15

---
+++ exercicios-praticos/exercicio-15/exercicio-livro-10.5/src/interfaces/Circulo.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+public class Circulo implements AreaCalculavel {
+    double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public double calculaArea() {
+        return Math.PI * raio * raio;
+    }
+}
