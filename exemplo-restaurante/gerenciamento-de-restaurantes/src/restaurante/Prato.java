741a6450888a2227329c7a73019ec6b7ef87f316
Luís André <andre2016999@gmail.com>
2024-10-08 23:47:55 -0300

    classe Prato

---
+++ exemplo-restaurante/gerenciamento-de-restaurantes/src/restaurante/Prato.java
@@ -0,0 +1,20 @@
+package restaurante;
+public abstract class Prato {
+    private String nome;
+    private double preco;
+
+    public Prato(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public abstract void preparar();
+}
