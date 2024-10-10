5b687a1987e3ae1dc04b5909bc2292383c3728d4
Luís André <andre2016999@gmail.com>
2024-10-09 23:24:20 -0300

    add classe Animal

---
+++ exemplo-veterinaria/veterinaria/src/veterinaria/Animal.java
@@ -0,0 +1,21 @@
+package veterinaria;
+
+public abstract class Animal {
+    private String nome;
+    private int idade;
+
+    public Animal(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public abstract void fazerSom();
+}
