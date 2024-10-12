24191232d0ccd53a6f146b1bddbadd481eee80de
Luís André <andre2016999@gmail.com>
2024-10-11 23:12:39 -0300

    exemplo generics

---
+++ collections/collections/src/generics/Carrinho.java
@@ -0,0 +1,32 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrinho<T> {
+    private List<T> itens;
+
+    public Carrinho() {
+        this.itens = new ArrayList<>();
+    }
+
+    public void adicionarItem(T item) {
+        itens.add(item);
+    }
+
+    public void removerItem(T item) {
+        itens.remove(item);
+    }
+
+    public void listarItens() {
+        System.out.println("\nItens no carrinho:");
+        for (T item : itens) {
+            System.out.println(item.toString());
+        }
+    }
+
+    public int contarItens() {
+        return itens.size();
+    }
+}
+
