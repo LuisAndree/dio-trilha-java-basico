d1c451f8dd01787049ecd90a3c6f62980e58b743
Luís André <andre2016999@gmail.com>
2024-10-12 21:32:36 -0300

    exemplo collections list - Membro

---
+++ collections/collections/src/list/Membro.java
@@ -0,0 +1,24 @@
+package list;
+
+public class Membro {
+    private String nome;
+    private String matricula;
+
+    public Membro(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "Membro: " + nome + ", Matrícula: " + matricula;
+    }
+}
