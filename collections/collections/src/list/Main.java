bfb1546e00686ce803bbea7fe0383d1809d9249c
Luís André <andre2016999@gmail.com>
2024-10-12 21:33:44 -0300

    exemplo collections list - RegistroMembros

---
+++ collections/collections/src/list/Main.java
@@ -0,0 +1,28 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("1984", "George Orwell", "123456789");
+        Livro livro2 = new Livro("O Senhor dos Anéis", "J.R.R. Tolkien", "987654321");
+
+        Membro membro1 = new Membro("João", "M001");
+
+        Biblioteca biblioteca = new Biblioteca("Biblioteca Central");
+        biblioteca.adicionarLivro(livro1);
+        biblioteca.adicionarLivro(livro2);
+        biblioteca.listarLivros();
+
+        RegistroMembros registro = new RegistroMembros();
+        registro.adicionarMembro(membro1);
+        registro.listarMembros();
+
+        List<Livro> livrosEmprestados = new ArrayList<>();
+        livrosEmprestados.add(livro1);
+
+        Emprestimo emprestimo = new Emprestimo(membro1, livrosEmprestados);
+        emprestimo.mostrarDetalhesEmprestimo();
+    }
+}
---
+++ collections/collections/src/list/RegistroMembros.java
@@ -0,0 +1,23 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegistroMembros {
+    private List<Membro> membros;
+
+    public RegistroMembros() {
+        this.membros = new ArrayList<>();
+    }
+
+    public void adicionarMembro(Membro membro) {
+        membros.add(membro);
+    }
+
+    public void listarMembros() {
+        System.out.println("\nMembros registrados:");
+        for (Membro membro : membros) {
+            System.out.println(membro);
+        }
+    }
+}
