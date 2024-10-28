72603cd67abba9f31249cb3e5328f21ed85955c9
Luís André <andre2016999@gmail.com>
2024-10-27 22:52:29 -0300

    exemplo biblioteca generics classe Livro

---
+++ collections/collections/src/generics/biblioteca/Livro.java
@@ -0,0 +1,40 @@
+package generics.biblioteca;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private boolean disponivel;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.disponivel = true;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro: " + titulo + " de " + autor + " (" + anoPublicacao + ")";
+    }
+}
