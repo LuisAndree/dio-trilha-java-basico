076b13dd7a2f19484719b90acedbef9ac59bed72
Luís André <andre2016999@gmail.com>
2024-10-28 19:08:08 -0300

    exemplo cardapiomenu generics classe Cliente

---
+++ collections/collections/src/generics/cardapiomenu/Cliente.java
@@ -0,0 +1,24 @@
+package generics.cardapiomenu;
+
+public class Cliente {
+    private String nome;
+    private int id;
+
+    public Cliente(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente: " + nome + " (ID: " + id + ")";
+    }
+}
