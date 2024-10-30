1347ce189f0469e79fbf968dd6db55d477291dcb
Luís André <andre2016999@gmail.com>
2024-10-29 23:31:11 -0300

    exemplo lojagames list classe Produto

---
+++ collections/collections/src/list/lojagames/Produto.java
@@ -0,0 +1,34 @@
+package list.lojagames;
+
+public abstract class Produto {
+    private String nome;
+    private double preco;
+    private int quantidadeEmEstoque;
+
+    public Produto(String nome, double preco, int quantidadeEmEstoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void atualizarEstoque(int quantidade) {
+        this.quantidadeEmEstoque += quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - R$" + preco + " | Estoque: " + quantidadeEmEstoque;
+    }
+}
