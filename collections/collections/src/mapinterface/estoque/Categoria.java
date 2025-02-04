46b67113f52d1a9d9757e56bc3d27d43546c01d3
Luís André <andre2016999@gmail.com>
2025-02-03 21:54:35 -0300

    exemplo estoque mapinterface classe Categoria

---
+++ collections/collections/src/mapinterface/estoque/Categoria.java
@@ -0,0 +1,18 @@
+package mapinterface.estoque;
+
+public class Categoria {
+    private String nome;
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
